62382053a0d0aaa1aca8ebc8abbab553aa971478
denis290788 <denis290788@gmail.com>
2019-12-15 21:28:45 +0300

    commit 1

--- src/test/java/CardDeliveryAppTest.java
+++ src/test/java/CardDeliveryAppTest.java
@@ -1,11 +1,12 @@
+import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 
 import java.text.SimpleDateFormat;
 import java.util.*;
 
-import static com.codeborne.selenide.Condition.exactText;
-import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
@@ -24,6 +25,30 @@ public class CardDeliveryAppTest {
         $("[data-test-id=city] input").setValue("Санкт-Петербург");
         $("[data-test-id=date] input").sendKeys(Keys.chord(Keys.CONTROL, "a", Keys.DELETE));
         $("[data-test-id=date] input").setValue(formatForDateNow.format(confirmedDate));
+        $("[data-test-id=name] input").setValue("Петрова Анна");
+        $("[data-test-id=phone] input").setValue("+79335843723");
+        $("[data-test-id=agreement]").click();
+        $(byText("Забронировать")).click();
+        $(byText("Успешно!")).waitUntil(visible, 15000);
+
+    }
+
+    @Test
+    void shouldConfirmCardDeliveryV2() {
+        Calendar instance = Calendar.getInstance();
+        Date dateNow = new Date();
+        SimpleDateFormat formatForDateNow = new SimpleDateFormat("dd.MM.yyyy");
+        instance.setTime(dateNow);
+        instance.add(Calendar.DAY_OF_MONTH, 3);
+        Date confirmedDate =  instance.getTime();
+
+        open("http://localhost:9999");
+        $("[data-test-id=city] input").setValue("Сан");
+        $$("[class=menu-item__control]").find(exactText("Санкт-Петербург")).click();
+        $("[data-test-id=date] input").sendKeys(Keys.chord(Keys.CONTROL, "a", Keys.DELETE));
+        $("[data-test-id=date] input").setValue(formatForDateNow.format(confirmedDate));
+//        $("[data-test-id=date] input").click();
+//        $$("[class=calendar-input__calendar-wrapper]").find(Condition.attribute("[class=\"calendar__day calendar__day_state_current\"]")).click();
         $("[data-test-id=name] input").setValue("Петрова Анна");
         $("[data-test-id=phone] input").setValue("+79335843723");
         $("[data-test-id=agreement]").click();
