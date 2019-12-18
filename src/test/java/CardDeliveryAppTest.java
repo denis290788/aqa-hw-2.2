a9865aa008f44dd9f3a944de9fb074bb061fc590
denis290788 <denis290788@gmail.com>
2019-12-18 21:58:41 +0300

    commit 1

--- src/test/java/CardDeliveryAppTest.java
+++ src/test/java/CardDeliveryAppTest.java
@@ -1,10 +1,9 @@
-import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 
 import java.text.SimpleDateFormat;
 import java.util.*;
+import java.time.LocalDate;
 
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.byText;
@@ -35,20 +34,17 @@ public class CardDeliveryAppTest {
 
     @Test
     void shouldConfirmCardDeliveryV2() {
-        Calendar instance = Calendar.getInstance();
-        Date dateNow = new Date();
-        SimpleDateFormat formatForDateNow = new SimpleDateFormat("dd.MM.yyyy");
-        instance.setTime(dateNow);
-        instance.add(Calendar.DAY_OF_MONTH, 3);
-        Date confirmedDate =  instance.getTime();
+        LocalDate currentDate = LocalDate.now().plusDays(7);
+        Date date = java.sql.Date.valueOf(currentDate);
+        //корректируем на время по Гринвичу
+        long dateInMillies = date.getTime() - 3600000 * 3;
+        String dateToCatch = Long.toString(dateInMillies);
 
         open("http://localhost:9999");
         $("[data-test-id=city] input").setValue("Сан");
         $$("[class=menu-item__control]").find(exactText("Санкт-Петербург")).click();
-        $("[data-test-id=date] input").sendKeys(Keys.chord(Keys.CONTROL, "a", Keys.DELETE));
-        $("[data-test-id=date] input").setValue(formatForDateNow.format(confirmedDate));
-//        $("[data-test-id=date] input").click();
-//        $$("[class=calendar-input__calendar-wrapper]").find(Condition.attribute("[class=\"calendar__day calendar__day_state_current\"]")).click();
+        $("[data-test-id=date] input").click();
+        $$("[class=calendar-input__calendar-wrapper]").find(exactText(dateToCatch)).click();
         $("[data-test-id=name] input").setValue("Петрова Анна");
         $("[data-test-id=phone] input").setValue("+79335843723");
         $("[data-test-id=agreement]").click();
